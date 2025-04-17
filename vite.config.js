a259da8642ac945e4cf10c43834884716ab07ff8
Penelope Yang <penelope.yang21@gmail.com>
2025-04-16 23:07:02 -0500

    Fix: set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/https://github.com/penelopeyang1/lab-quiz.git/', 
+  base: '/lab-quiz.git/', 
   plugins: [react()],
 })
