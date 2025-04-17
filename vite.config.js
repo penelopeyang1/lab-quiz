e122c39ec4383cf4210b325990ba3d3734e27379
Penelope Yang <penelope.yang21@gmail.com>
2025-04-16 23:09:32 -0500

    fixed a tiny error

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/lab-quiz.git/', 
+  base: '/lab-quiz/', 
   plugins: [react()],
 })
